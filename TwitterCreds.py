ebd3a613f2ea64154e3db31078e5024f5c4ee1e3
Shawn Grimes <shawn@campfireapps.com>
2014-01-06 22:37:27 -0500

    Moved credentials to an external file for better repository control

---
+++ TwitterCreds.py
@@ -0,0 +1,18 @@
+
+
+# Twitter application authentication
+def APP_KEY(): 
+	APP_KEY = 'YOUR_APP_KEY'
+	return APP_KEY
+
+def APP_SECRET(): 
+	APP_SECRET = 'YOUR_APP_SECRET'
+	return APP_SECRET
+
+def OAUTH_TOKEN(): 
+	OAUTH_TOKEN = 'YOUR_OAUTH_TOKEN'
+	return OAUTH_TOKEN
+
+def OAUTH_TOKEN_SECRET(): 
+	OAUTH_TOKEN_SECRET = 'YOUR_OAUTH_TOKEN_SECRET'
+	return OAUTH_TOKEN_SECRET
